1f41d4e25259d2a12fedd67a0fe260b1d80d0fcf
li772091958 <772091958@qq.com>
2016-04-27 15:25:15 +0800

    Create contact.js

---
+++ html/js/contact.js
@@ -0,0 +1,17 @@
+$("input[name='submit']").click(function(){
+	AV.initialize('PsLiU3e6sAFkoOnaBRS1BThk-gzGzoHsz', 'WQ2jeDbbVWlEnAJ3iWymS4Px');
+	var TestObject = AV.Object.extend('TestObject');
+	var testObject = new TestObject();
+	var name = $("#Name").val();
+	var email = $("#Email").val();
+	var message = $("#Message").val();
+	testObject.save({
+		name : name,
+		email : email,
+		message : message
+	},{
+		success: function(object) {
+			alert('留言成功，我会及时查看并予以回复！');
+		}
+	});
+})
